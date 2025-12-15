74789bc5dc899484f5208c88c116984b9a7ac20c
Stefan Lüthi <stefan.luethi@sourceengineers.com>
2025-12-15 09:32:46 +0100

    build: add install targets

--- cmake/resource_database.cmake
+++ cmake/resource_database.cmake
@@ -14,7 +14,10 @@ function(se_create_resource_database)
 
     find_package(Python3)
 
-    set(GENERATED_SOURCE ${CMAKE_CURRENT_BINARY_DIR}/SeResourceDatabase.cpp)
+    set(TEMPLATE_SOURCE_FILE ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../tools/ResourceDatabase.cpp.in)
+    set(GENERATED_SOURCE_FILE ${CMAKE_CURRENT_BINARY_DIR}/SeResourceDatabase.cpp)
+    set(GENERATED_DATABASE_FILE ${CMAKE_CURRENT_BINARY_DIR}/se_log_resource_database.json)
+
     separate_arguments(COMPILER_FLAGS NATIVE_COMMAND ${CMAKE_CXX_FLAGS})
 
     add_custom_command(
@@ -22,9 +25,9 @@ function(se_create_resource_database)
             TARGET ${SE_LOG_DB_TARGET}
             PRE_LINK
             COMMAND echo "[se-log] Creating database for log messages"
-            COMMAND ${CMAKE_NM} -uC $<TARGET_OBJECTS:${SE_LOG_DB_TARGET}> | python3 ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../tools/create_resource_database.py ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../tools/ResourceDatabase.cpp.in ${GENERATED_SOURCE} ${CMAKE_CURRENT_BINARY_DIR}/se_log_resource_database.json
+            COMMAND ${CMAKE_NM} -uC $<TARGET_OBJECTS:${SE_LOG_DB_TARGET}> | python3 ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../tools/create_resource_database.py ${TEMPLATE_SOURCE_FILE} ${GENERATED_SOURCE_FILE} ${GENERATED_DATABASE_FILE}
             # In order to prevent a dependency cycle we compile the file manually and do not add it to BYPRODUCTS
-            COMMAND ${CMAKE_CXX_COMPILER} -I ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../include ${COMPILER_FLAGS} -c ${GENERATED_SOURCE} -o ${GENERATED_SOURCE}.o
+            COMMAND ${CMAKE_CXX_COMPILER} -I ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../include ${COMPILER_FLAGS} -c ${GENERATED_SOURCE_FILE} -o ${GENERATED_SOURCE_FILE}.o
             VERBATIM
             COMMAND_EXPAND_LISTS
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
@@ -33,6 +36,8 @@ function(se_create_resource_database)
     target_link_options(
             ${SE_LOG_DB_TARGET}
             PUBLIC
-            ${GENERATED_SOURCE}.o
+            ${GENERATED_SOURCE_FILE}.o
     )
+    
+    install(FILES ${GENERATED_DATABASE_FILE} DESTINATION ${CMAKE_INSTALL_PREFIX} COMPONENT firmware)
 endfunction()
