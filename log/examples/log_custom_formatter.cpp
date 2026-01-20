80acd32ee42fe936d5666d660b0e0518e289a7ee
Stefan Lüthi <stefan.luethi@sourceengineers.com>
2026-01-20 11:09:45 +0100

    feat(log): add custom formatter example

---
+++ log/examples/log_custom_formatter.cpp
@@ -0,0 +1,80 @@
+/*
+* Copyright (c) 2025 Source Engineers GmbH
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "LogComponents.h"
+#include "se-oss/log/Log.h"
+#include "se-oss/log/LogRegistry.h"
+#include "se-oss/log/sink/ConsoleSink.h"
+#include "se-oss/log/sink/FilteredSink.h"
+
+#include <chrono>
+
+// Configure logger
+constexpr std::size_t LOG_BUFFER_SIZE {2048U};
+constexpr std::size_t LOG_MAX_MESSAGE_LENGTH {128U};
+
+class MyFormatter
+{
+public:
+    template<typename... Values>
+    static size_t format(void* buffer, std::size_t bufferSize, const se_oss::LogRecord& record, const char *const formatString, const Values& ...values)
+    {
+        se_oss::LogStringBuffer string {buffer, bufferSize};
+        string.appendTime("%y%m%dT%H%M%S", record.timestamp);
+        string.append("| %s | ", toString(record.metadata.level));
+        string.append("%s | ", record.sourceName);
+        string.append(formatString, std::forward<const Values>(values)...);
+        return string.length();
+    }
+};
+
+template <>
+auto se_oss::logConf<>()
+{
+    return LogConf<MyFormatter, AtomicBuffer<LOG_BUFFER_SIZE>, LOG_MAX_MESSAGE_LENGTH>{};
+}
+
+int main()
+{
+    auto consoleSink = std::make_unique<se_oss::FilteredSink<se_oss::ConsoleSink>>();
+
+    auto logRegistry = std::make_unique<se_oss::LogRegistry<LogComponents, LogSinks>>();
+    logRegistry->setTimeProvider([]() {
+        return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    });
+    logRegistry->attachSink(LogSinks::SHELL, std::move(consoleSink));
+    logRegistry->getSink(LogSinks::SHELL).setLogLevel(se_oss::LogLevel::TRACE);
+
+    se_oss::Logger& logger = logRegistry->createOrGetLogger(LogComponents::CELLULAR);
+
+    logger.setLogLevel(se_oss::LogLevel::TRACE);
+
+    LOG_TRACE(logger, "trace");
+    LOG_DEBUG(logger, "debug");
+    LOG_INFO(logger, "info");
+    LOG_WARN(logger, "warn");
+    LOG_ERROR(logger, "error");
+    LOG_FATAL(logger, "fatal");
+
+    for (uint32_t i = 0; i < 10; ++i) {
+        LOG_INFO(logger, "hello %u", i);
+    }
+
+    logRegistry->distributeMessages();
+
+    LOG_INFO(logger, "values %u", 1U);
+    LOG_INFO(logger, "values %u, %u", 1U, 2U);
+    LOG_INFO(logger, "values %u, %u, %u", 1U, 2U, 3U);
+    LOG_INFO(logger, "values %u, %u, %u, %u", 1U, 2U, 3U, 4U);
+    LOG_INFO(logger, "values with floats %f", 42.0F);
+    LOG_INFO(logger, "values with floats %f, %u", 42.0F, 2U);
+    LOG_INFO(logger, "values negative %f, %d", -42.0F, -2);
+    LOG_INFO(logger, "values string %s", "hello");
+
+    logRegistry->distributeMessages();
+
+    return 0;
+}
