7d228d61b85ffcf995c50c5b62fa40fe98e46997
Stefan Lüthi <stefan.luethi@sourceengineers.com>
2026-02-16 11:26:36 +0100

    build: remove hard-coded target name from cmake function

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -4,10 +4,22 @@
 find_program(GCOVR gcovr)
 
 function(se_coverage)
+    # parse input arguments
+    set(prefix COV)
+    set(singleValues PROJECT_NAME)
+    set(multiValues)
+
+    include(CMakeParseArguments)
+    cmake_parse_arguments(${prefix}
+            "${flags}"
+            "${singleValues}"
+            "${multiValues}"
+            ${ARGN})
+
     add_custom_target(
-            se_oss-coverage
+            ${}-coverage
             COMMAND mkdir -p ${CMAKE_BINARY_DIR}/report/coverage
-            COMMAND ${GCOVR} --xml-pretty --exclude-unreachable-branches --print-summary -o ${CMAKE_BINARY_DIR}/report/se-oss-test-coverage-report.xml --html --html-theme github.dark-green --html-details ${CMAKE_BINARY_DIR}/report/coverage/index.html --root ${CMAKE_SOURCE_DIR} -e /.*/build/ -e /.*/test/ -e /usr* -e .*/lib/
+            COMMAND ${GCOVR} --xml-pretty --exclude-unreachable-branches --print-summary -o ${CMAKE_BINARY_DIR}/report/${PROJECT_NAME}-coverage-report.xml --html --html-theme github.dark-green --html-details ${CMAKE_BINARY_DIR}/report/coverage/index.html --root ${CMAKE_SOURCE_DIR} -e /.*/build/ -e /.*/test/ -e /usr* -e .*/lib/
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMENT "Create coverage report"
             VERBATIM
